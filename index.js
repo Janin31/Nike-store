6d195fbdfaa9d21c644164c381d430e0b2bb4671
Janin31 <145654443+Janin31@users.noreply.github.com>
2023-10-26 10:20:53 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ const products = [
     colors: [
       {
         code: "black",
-        img: "/images/air.png",
+        img: "air.png",
       },
       {
         code: "darkblue",
-        img: "/images/air2.png",
+        img: "air2.png",
       },
     ],
   },
@@ -24,11 +24,11 @@ const products = [
     colors: [
       {
         code: "lightgray",
-        img: "/images/jordan.png",
+        img: "jordan.png",
       },
       {
         code: "green",
-        img: "/images/jordan2.png",
+        img: "jordan2.png",
       },
     ],
   },
@@ -39,11 +39,11 @@ const products = [
     colors: [
       {
         code: "lightgray",
-        img: "/images/blazer.png",
+        img: "blazer.png",
       },
       {
         code: "green",
-        img: "/images/blazer2.png",
+        img: "blazer2.png",
       },
     ],
   },
@@ -54,11 +54,11 @@ const products = [
     colors: [
       {
         code: "black",
-        img: "/images/crater.png",
+        img: "crater.png",
       },
       {
         code: "lightgray",
-        img: "/images/crater2.png",
+        img: "crater2.png",
       },
     ],
   },
@@ -69,11 +69,11 @@ const products = [
     colors: [
       {
         code: "gray",
-        img: "/images/hippie.png",
+        img: "hippie.png",
       },
       {
         code: "black",
-        img: "/images/hippie2.png",
+        img: "hippie2.png",
       },
     ],
   },
